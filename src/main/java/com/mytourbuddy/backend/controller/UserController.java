00e34895840d7722e6c45ba4c990d0a87dc01158
Sasmitha Ashinsana <sinhalayapage3@gmail.com>
2026-01-02 16:53:41 +0530

    feat: add listner to update isProfileComplete

--- src/main/java/com/mytourbuddy/backend/controller/UserController.java
+++ src/main/java/com/mytourbuddy/backend/controller/UserController.java
@@ -1,10 +1,20 @@
 package com.mytourbuddy.backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.mytourbuddy.backend.dto.request.RegisterRequest;
 import com.mytourbuddy.backend.dto.request.UpdateRequest;
@@ -12,8 +22,6 @@ import com.mytourbuddy.backend.dto.response.UserResponse;
 import com.mytourbuddy.backend.service.UserService;
 
 import jakarta.validation.Valid;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -57,5 +65,4 @@ public class UserController {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
-
 }
